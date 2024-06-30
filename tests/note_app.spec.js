de643d3411c1b82e079adfbd160dfaf3a9374fb9
zntb <zenetibi@yahoo.com>
2024-06-30 10:14:43 +0300

    End to end testing: Playwright, testing our own code, writing on the form

---
+++ tests/note_app.spec.js
@@ -0,0 +1,22 @@
+const { test, describe, expect, beforeEach } = require('@playwright/test')
+
+describe('Note app', () => {
+
+  beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:5173')
+  })
+
+  test('front page can be opened', async ({ page }) => {
+    const locator = await page.getByText('Notes')
+    await expect(locator).toBeVisible()
+    await expect(page.getByText('Note app, Department of Computer Science, University of Helsinki 2024')).toBeVisible()
+  })
+
+  test('login form can be opened', async ({ page }) => {
+    await page.getByRole('button', { name: 'log in' }).click()
+    await page.getByTestId('username').fill('root')
+    await page.getByTestId('password').fill('salainen')
+    await page.getByRole('button', { name: 'login' }).click()
+    await expect(page.getByText('Superuser logged in')).toBeVisible()
+  })
+})
