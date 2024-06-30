602852123411cc315994e01862f6ade258eb41c4
zntb <zenetibi@yahoo.com>
2024-06-30 11:08:18 +0300

    Testing note creation, controlling the state of the database

--- tests/note_app.spec.js
+++ tests/note_app.spec.js
@@ -1,8 +1,16 @@
 const { test, describe, expect, beforeEach } = require('@playwright/test')
 
 describe('Note app', () => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('http:localhost:3001/api/testing/reset')
+    await request.post('http://localhost:3001/api/users', {
+      data: {
+        name: 'Matti Luukkainen',
+        username: 'mluukkai',
+        password: 'salainen'
+      }
+    })
 
-  beforeEach(async ({ page }) => {
     await page.goto('http://localhost:5173')
   })
 
@@ -14,9 +22,39 @@ describe('Note app', () => {
 
   test('login form can be opened', async ({ page }) => {
     await page.getByRole('button', { name: 'log in' }).click()
-    await page.getByTestId('username').fill('root')
+    await page.getByTestId('username').fill('mluukkai')
     await page.getByTestId('password').fill('salainen')
     await page.getByRole('button', { name: 'login' }).click()
-    await expect(page.getByText('Superuser logged in')).toBeVisible()
+  
+    await expect(page.getByText('Matti Luukkainen logged in')).toBeVisible()
   })
+
+  describe('when logged in', () => {
+    beforeEach(async ({ page }) => {
+      await page.getByRole('button', { name: 'log in' }).click()
+      await page.getByTestId('username').fill('mluukkai')
+      await page.getByTestId('password').fill('salainen')
+      await page.getByRole('button', { name: 'login' }).click()
+    })
+
+    test('a new note can be created', async ({ page }) => {
+      await page.getByRole('button', { name: 'new note' }).click()
+      await page.getByRole('textbox').fill('a note created by playwright')
+      await page.getByRole('button', { name: 'save' }).click()
+      await expect(await page.getByText('a note created by playwright')).toBeVisible()
+    })
+
+    describe('and a note exists', () => {
+      beforeEach(async ({ page }) => {
+        await page.getByRole('button', { name: 'new note' }).click()
+        await page.getByRole('textbox').fill('another note by playwright')
+        await page.getByRole('button', { name: 'save' }).click()
+      })
+  
+      test('importance can be changed', async ({ page }) => {
+        await page.getByRole('button', { name: 'make not important' }).click()
+        await expect(await page.getByText('make important')).toBeVisible()
+      })
+    })
+  })  
 })
